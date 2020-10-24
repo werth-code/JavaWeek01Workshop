c15317c2c8c7c1638b6f82e6c7a74c579f2eefd6
werth-code <matthewwerth@gmail.com>
2020-10-24 20:27:04 +0000

    Updated methods

--- src/main/java/com/codedifferently/part01/Greetings.java
+++ src/main/java/com/codedifferently/part01/Greetings.java
@@ -1,7 +1,30 @@
+// package com.codedifferently.part01;
+
+// public class Greetings {
+//     public static void main(String[] args) {
+
+//         System.out.println("What's your name?  ");
+//         String name = System.console().readLine();
+
+//         if(name.equalsIgnoreCase("hakim") || name.equalsIgnoreCase("Rasheeda")) {
+//             System.out.println("Hi " + name);
+//         }
+//     } 
+// }
+
 package com.codedifferently.part01;
+
+import java.util.Scanner;
 
 public class Greetings {
     public static void main(String[] args) {
+        System.out.println("What's Your Name  ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
 
+        if(name.equalsIgnoreCase("hakim") || name.equalsIgnoreCase("Rasheeda")) {
+            System.out.println("Hi " + name);
+        }
     }
+
 }
