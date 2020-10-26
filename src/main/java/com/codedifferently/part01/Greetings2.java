b1976749cff19650fdfd93a97b6e7e131bd122f4
werth-code <matthewwerth@gmail.com>
2020-10-26 13:57:06 +0000

    Added Guess A Number Game

--- src/main/java/com/codedifferently/part01/Greetings2.java
+++ src/main/java/com/codedifferently/part01/Greetings2.java
@@ -3,7 +3,7 @@ package com.codedifferently.part01;
 public class Greetings2 {
 
     public static void main(String[] args) {
-        Book jamesMaddison = new Book("James Maddison", "Hello America", 455);
+        Book jamesMaddison = new Book("James Maddison", "Hello America", 455, true);
         System.out.println("The Book Is " + jamesMaddison.title + ". It has " + jamesMaddison.noPages + " pages");
     }
 
--- src/main/java/com/codedifferently/part02/TooHighTooLow.java
+++ src/main/java/com/codedifferently/part02/TooHighTooLow.java
@@ -1,8 +1,22 @@
 package com.codedifferently.part02;
+import java.util.Random;
+import static java.lang.Integer.parseInt;
 
 public class TooHighTooLow {
 
     public static void main(String[] args) {
+        int randomNumber = new Random().nextInt(50 + 1);
+        int userResponse;
 
+        do {
+            System.out.println("Guess A Number Between 0 & 50 ");
+            userResponse = parseInt(System.console().readLine());
+
+            if (userResponse < randomNumber) System.out.println("You Guessed Too Low, Guess Again!");
+            else System.out.println("You Guessed Too High, Guess Again!");
+        
+        } while (userResponse != randomNumber);
+
+        System.out.printf("%d Is The Right Number!%n%n", userResponse);
     }
 }
