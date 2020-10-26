b02fa601d1d3e07ce3e3747e189e8017456bdf67
werth-code <matthewwerth@gmail.com>
2020-10-26 18:55:30 +0000

    Added Guess Counter And Past Guess Tracking

--- src/main/java/com/codedifferently/part02/TooHighTooLow.java
+++ src/main/java/com/codedifferently/part02/TooHighTooLow.java
@@ -1,22 +1,34 @@
 package com.codedifferently.part02;
 import java.util.Random;
 import static java.lang.Integer.parseInt;
+import java.util.ArrayList;
 
 public class TooHighTooLow {
 
     public static void main(String[] args) {
         int randomNumber = new Random().nextInt(50 + 1);
         int userResponse;
+        int guessCount = 0;
+        ArrayList<Integer> pastGuesses = new ArrayList<Integer>();
 
         do {
             System.out.println("Guess A Number Between 1 & 50 ");
             userResponse = parseInt(System.console().readLine());
+            
+            if(pastGuesses.indexOf(userResponse) == -1) {
+                guessCount++;
+                pastGuesses.add(userResponse);
+            } else System.out.println("You already guessed that one.");
+            
+            if (userResponse < randomNumber) {
+                System.out.println("You Guessed Too Low, Guess Again!");    
+            }
+            if (userResponse > randomNumber) {
+                System.out.println("You Guessed Too High, Guess Again!");
+            }
 
-            if (userResponse < randomNumber) System.out.println("You Guessed Too Low, Guess Again!");
-            else System.out.println("You Guessed Too High, Guess Again!");
-        
         } while (userResponse != randomNumber);
 
-        System.out.printf("%d Is The Right Number!%n%n", userResponse);
+        System.out.printf("%d Is The Right Number!%nIt Took You %d Tries.%n", userResponse, guessCount);
     }
 }
